3c05adc483272a609479550016359fa72e0b258a
mmadyda <63304339+mmadyda@users.noreply.github.com>
2023-05-12 10:47:20 +0200

    Update Maszyna.java
    POP

--- src/cykleapp/Maszyna.java
+++ src/cykleapp/Maszyna.java
@@ -38,14 +38,14 @@ public class Maszyna {
     public static String STYL_BRAK_DANYCH = (
                                 "    -fx-background-color: linear-gradient(derive(#1b1b1b,+80%), derive(#1b1b1b,+90%)),\n" +
                                 "                          linear-gradient(derive(#1b1b1b,20%), derive(#1b1b1b, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#1b1b1b,-15%), derive(#1b1b1b,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#1b1b1b,-5%), derive(#1b1b1b,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
 
     public static String STYL_BPRACA = (
                                 "    -fx-background-color: linear-gradient(derive(#32cd32,+80%), derive(#32cd32,+90%)),\n" +
                                 "                          linear-gradient(derive(#32cd32,20%), derive(#32cd32, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 100%,derive(#32cd32,-15%), derive(#32cd32, 30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#32cd32,-5%), derive(#32cd32, 30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
@@ -53,28 +53,28 @@ public class Maszyna {
     public static String STYL_PROBY_TECHNOLOGICZNE = (
                                 "    -fx-background-color: linear-gradient(derive(#8db600,+80%), derive(#8db600,+90%)),\n" +
                                 "                          linear-gradient(derive(#8db600,20%), derive(#8db600, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8db600,-15%), derive(#8db600,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8db600,-5%), derive(#8db600,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
     public static String STYL_POSTOJ_PLANOWANY = (
                                 "    -fx-background-color: linear-gradient(derive(#cccccc,+80%), derive(#cccccc,+90%)),\n" +
                                 "                          linear-gradient(derive(#cccccc,20%), derive(#cccccc, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#cccccc,-15%), derive(#cccccc,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#cccccc,-5%), derive(#cccccc,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
      
     public static String STYL_PRZEZBRAJANIE = (
                                 "    -fx-background-color: linear-gradient(derive(#22bad9,+80%), derive(#22bad9,+90%)),\n" +
                                 "                          linear-gradient(derive(#22bad9,20%), derive(#22bad9, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#22bad9,-15%), derive(#22bad9,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#22bad9,-5%), derive(#22bad9,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
     public static String STYL_SUSZ_M = (
                                 "    -fx-background-color: linear-gradient(derive(#8d6e63,+80%), derive(#8d6e63,+90%)),\n" +
                                 "                          linear-gradient(derive(#8d6e63,20%), derive(#8d6e63, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8d6e63,-15%), derive(#8d6e63,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8d6e63,-5%), derive(#8d6e63,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
      
@@ -87,27 +87,27 @@ public class Maszyna {
     public static String STYL_AWARIA_MASZYNY = (
                                 "    -fx-background-color: linear-gradient(derive(#dc143c,+80%), derive(#dc143c,+90%)),\n" +
                                 "                          linear-gradient(derive(#dc143c,20%), derive(#dc143c, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#dc143c,-15%), derive(#dc143c,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#dc143c,-5%), derive(#dc143c,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
     public static String STYL_AWARIA_FORMY = (
                                 "    -fx-background-color: linear-gradient(derive(#ff6347,+80%), derive(#ff6347,+90%)),\n" +
                                 "                          linear-gradient(derive(#ff6347,20%), derive(#ff6347, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ff6347,-15%), derive(#ff6347,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ff6347,-5%), derive(#ff6347,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     public static String STYL_BRAK_ZAOPATRZENIA = (
                                 "    -fx-background-color: linear-gradient(derive(#ffd700,+80%), derive(#ffd700,+90%)),\n" +
                                 "                          linear-gradient(derive(#ffd700,20%), derive(#ffd700, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ffd700,-15%), derive(#ffd700,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ffd700,-5%), derive(#ffd700,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
     public static String STYL_PRZERWA_P = (
                                 "    -fx-background-color: linear-gradient(derive(#008080,+80%), derive(#008080,+90%)),\n" +
                                 "                          linear-gradient(derive(#008080,20%), derive(#008080, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#008080,-15%), derive(#008080,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#008080,-5%), derive(#008080,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
@@ -115,7 +115,7 @@ public class Maszyna {
     public static String STYL_BRAK_OPERATORA = (
                                 "    -fx-background-color: linear-gradient(derive(#8e44ad,+80%), derive(#8e44ad,+90%)),\n" +
                                 "                          linear-gradient(derive(#8e44ad,20%), derive(#8e44ad, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8e44ad,-15%), derive(#8e44ad,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#8e44ad,-5%), derive(#8e44ad,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
      
@@ -123,7 +123,7 @@ public class Maszyna {
     public static String STYL_WYBRAK = (
                                 "    -fx-background-color: linear-gradient(derive(#ff8c00,+80%), derive(#ff8c00,+90%)),\n" +
                                 "                          linear-gradient(derive(#ff8c00,20%), derive(#ff8c00, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ff8c00,-15%), derive(#ff8c00,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ff8c00,-5%), derive(#ff8c00,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
     
@@ -131,7 +131,7 @@ public class Maszyna {
      public static String STYL_NIE_PODLACZONE = (
                                 "    -fx-background-color: linear-gradient(derive(#ffffff,-80%), derive(#ffffff,-90%)),\n" +
                                 "                          linear-gradient(derive(#ffffff,20%), derive(#ffffff, 30%)),\n" +
-                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ffffff,-15%), derive(#ffffff,30%));\n" +
+                                "                          radial-gradient(center 50% 50%,radius 120%,derive(#ffffff,-5%), derive(#ffffff,30%));\n" +
                                 "    -fx-background-insets: 0,1,2;\n" +
                                 "    -fx-background-radius: 3 3 3 3, 3 3 3 3, 3 3 3 3;\n");
      
